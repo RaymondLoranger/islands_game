26289a3a470581f37cbd018624b67ddebe645f28
RaymondLoranger <Raymond.Loranger@bell.net>
2020-03-29 20:29:42 -0400

    0.1.8

--- test/islands/game_test.exs
+++ test/islands/game_test.exs
@@ -10,24 +10,24 @@ defmodule Islands.GameTest do
     eden = Game.new("Eden", "Adam", :m, this)
 
     poison =
-      ~s<{"state":{"player2_state":"islands_not_set","player1_state":"islands_not_set","game_state":"initialized"},"response":[],"request":[],"player2":{"name":"?","guesses":{"misses":[],"hits":[]},"gender":"f","board":{"misses":[],"islands":{}}},"player1":{"name":"Adam","guesses":{"misses":[],"hits":[]},"gender":"m","board":{"misses":[],"islands":{}}},"name":"Eden"}>
+      ~s<{"state":{"player2_state":"islands_not_set","player1_state":"islands_not_set","game_state":"initialized"},"response":[],"request":[],"player2":{"name":"?","guesses":null,"gender":"f","board":null},"player1":{"name":"Adam","guesses":null,"gender":"m","board":null},"name":"Eden"}>
 
     jason =
-      ~s<{"name":"Eden","player1":{"name":"Adam","gender":"m","board":{"islands":{},"misses":[]},"guesses":{"hits":[],"misses":[]}},"player2":{"name":"?","gender":"f","board":{"islands":{},"misses":[]},"guesses":{"hits":[],"misses":[]}},"request":[],"response":[],"state":{"game_state":"initialized","player1_state":"islands_not_set","player2_state":"islands_not_set"}}>
+      ~s<{"name":"Eden","player1":{"name":"Adam","gender":"m","board":null,"guesses":null},"player2":{"name":"?","gender":"f","board":null,"guesses":null},"request":[],"response":[],"state":{"game_state":"initialized","player1_state":"islands_not_set","player2_state":"islands_not_set"}}>
 
     decoded = %{
       "name" => "Eden",
       "player1" => %{
         "gender" => "m",
         "name" => "Adam",
-        "board" => %{"islands" => %{}, "misses" => []},
-        "guesses" => %{"hits" => [], "misses" => []}
+        "board" => nil,
+        "guesses" => nil
       },
       "player2" => %{
         "gender" => "f",
         "name" => "?",
-        "board" => %{"islands" => %{}, "misses" => []},
-        "guesses" => %{"hits" => [], "misses" => []}
+        "board" => nil,
+        "guesses" => nil
       },
       "request" => [],
       "response" => [],
