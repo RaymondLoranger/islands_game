cbae19002b1a6c2ec26f68d63d135752f9264427
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-25 14:32:48 -0500

    0.1.43

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Game.MixProject do
   def project do
     [
       app: :islands_game,
-      version: "0.1.42",
+      version: "0.1.43",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Game",
@@ -37,7 +37,7 @@ defmodule Islands.Game.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:crypto, :logger]
+      extra_applications: [:logger]
     ]
   end
 
